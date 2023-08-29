b6604d7efadb62b55476057212ea33b980ee8b7d
Flávio Rosa <flavio.rosa@turbinekreuzberg.com>
2023-08-29 15:04:42 +0100

    introduce Gitlab service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Services\GitLab\Config\ChicletsConfig;
+use App\Services\GitLab\GitLabService;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +14,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            GitLabService::class,
+            function (Application $app) {
+                return new GitLabService($app->make(ChicletsConfig::class));
+            },
+        );
+
+        $this->app->bind(
+            ChicletsConfig::class,
+            function () {
+                return new ChicletsConfig();
+            },
+        );
     }
 
     /**
--- app/Services/GitLab/GitLabService.php
+++ app/Services/GitLab/GitLabService.php
@@ -2,12 +2,18 @@
 
 namespace App\Services\GitLab;
 
-use App\Services\Config\ConfigInterface;
+use App\Services\GitLab\Config\ConfigInterface;
+use App\Services\GitLab\Model\Pipeline;
 use App\Services\GitLab\Model\PipelineCollection;
+use App\Services\GitLab\Model\Project;
 use App\Services\GitLab\Model\ProjectCollection;
+use GuzzleHttp\Client;
 
 class GitLabService implements GitlabServiceInterface
 {
+    private const GET_PIPELINES_URL_PATTERN = '/api/v4/projects/%s/pipelines';
+    private const GET_PROJECTS_URL = '/api/v4/projects';
+
     public function __construct(private ConfigInterface $config)
     {
 
@@ -15,23 +21,44 @@ class GitLabService implements GitlabServiceInterface
 
     public function getPipelines(): PipelineCollection
     {
-        // make request
-        // prepare response
-        return new PipelineCollection();
+        $url = sprintf(self::GET_PIPELINES_URL_PATTERN, $this->config->getCurrentProjectId());
+        $response = json_decode($this->getConnection()->get($url)->getBody(), true, 512, JSON_THROW_ON_ERROR);
+
+        $pipelineCollection = new PipelineCollection();
+        foreach ($response as $pipeline) {
+            $pipelineObj = new Pipeline($pipeline);
+
+            $pipelineCollection[] = $pipelineObj;
+        }
+
+        return $pipelineCollection;
     }
 
     public function getProjects(): ProjectCollection
     {
-        // make request
-        // prepare response
-        return new ProjectCollection();
+        $response = json_decode($this->getConnection()->get(self::GET_PROJECTS_URL)->getBody(), true, 512, JSON_THROW_ON_ERROR);
+
+        $projectCollection = new ProjectCollection();
+        foreach ($response as $project) {
+            $projectObj = new Project($project);
+
+            $projectCollection[] = $projectObj;
+        }
+
+        return $projectCollection;
     }
 
-    private function getConnection(): ConnectionInterface {
+    private function getConnection(): Client {
         static $connection = null;
         if ($connection === null) {
-            $connection = new Client($this->config->getBaseUrl());
+            $connection = new Client([
+                'base_uri' => $this->config->getBaseUrl(),
+                'headers' => [
+                    'PRIVATE-TOKEN' => $this->config->getToken(),
+                ],
+            ]);
         }
+
         return $connection;
     }
 }
